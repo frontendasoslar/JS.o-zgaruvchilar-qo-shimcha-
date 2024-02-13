97566b588a2cbfe0daa0689a20ae054e7c3540b3
frondendasoslar <faxriddin19951021@gmai.com>
2024-02-13 14:31:18 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var username = ("Faxriddin"); //string ya'ni matn turi
+// username o'zgaruvchi elon qilinayapdi va unga Faxriddin qiymatini bog'lab qo'yayapdi
+console.log(username);
+//username o'zgaruvchisini elon qilayapdi ya'ni consolda Faxriddin chiqadi
+alert(username)
+//alert-tegi o'zgaruvchini browserda alohida kartochkada chiqaradi ok tugmasi bosilsa kartochka yopladi
